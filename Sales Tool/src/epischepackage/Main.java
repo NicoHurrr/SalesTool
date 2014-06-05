8806f4748e716b6c900e124e0506b2f993e6e687
RobinRaawr <Rkok94@gmail.com>
2014-06-05 17:26:38 +0200

    println did I mention a fgt?

--- Sales Tool/src/epischepackage/Main.java
+++ Sales Tool/src/epischepackage/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("FGT");
 		System.err.print("Nico sehr fgt");
+		System.out.println("did I mention fgt?");
 	}
 
 }
